c43ff064fabfb131aeaed1abea2df60ba5c6f094
Administrator <Administrator@MS-20170130XTOR>
2018-03-04 20:56:47 +0800

    oo

--- com.token/src/main/java/cn/itcast/TokenFilter.java
+++ com.token/src/main/java/cn/itcast/TokenFilter.java
@@ -1,7 +1,5 @@
 package cn.itcast;
 
-import io.jsonwebtoken.Claims;
-
 import java.io.IOException;
 
 import javax.servlet.Filter;
@@ -15,74 +13,84 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.alibaba.fastjson.JSONObject;
 
 import cn.itcast.redis.RedisApi;
+
 //@Component
-public class TokenFilter implements Filter{
-   private static Logger logger = Logger.getLogger(TokenFilter.class); 
-	private String excludedPages;
-	private String[] excludedPageArray;
-	private String redirectPath;
-	/*@Autowired
-	private JwtTokenProvider jwtTokenProvider;*/
+public class TokenFilter implements Filter {
+	private static Logger logger = Logger.getLogger(TokenFilter.class);
+	/*
+	 * private String excludedPages; private String[] excludedPageArray; private
+	 * String redirectPath;
+	 */
+	private FilterConfig filterConfig;
+
+	/*
+	 * @Autowired private JwtTokenProvider jwtTokenProvider;
+	 */
 	public void init(FilterConfig filterConfig) throws ServletException {
-		excludedPages = filterConfig.getInitParameter("excludedPages");
-		if (StringUtils.isNotEmpty(excludedPages)) {     
-			excludedPageArray = excludedPages.split(",");     
-			} 
-		redirectPath = filterConfig.getInitParameter("redirectPath");
+		/*
+		 * excludedPages = filterConfig.getInitParameter("excludedPages"); if
+		 * (StringUtils.isNotEmpty(excludedPages)) { excludedPageArray =
+		 * excludedPages.split(","); } redirectPath =
+		 * filterConfig.getInitParameter("redirectPath");
+		 */
+		this.filterConfig = filterConfig;
 	}
 
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 		logger.info("start test:");
-		boolean isExcludedPage = false;     
+		boolean isExcludedPage = false;
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
+		String excludedPages = filterConfig.getInitParameter("excludedPages");
+		String[] excludedPageArray = null;
+		if (StringUtils.isNotEmpty(excludedPages)) {
+			excludedPageArray = excludedPages.split(",");
+		}
+		String redirectPath = filterConfig.getInitParameter("redirectPath");
 		String loginPath = req.getContextPath() + redirectPath;// 没有登陆转向页面
 		String uri = req.getRequestURI();
-		uri = uri.replace(req.getContextPath(),"");
+		uri = uri.replace(req.getContextPath(), "");
 		for (String page : excludedPageArray) {// 判断是否在过滤url之外
 			if (uri.equals(page)) {
 				isExcludedPage = true;
 				break;
 			}
-		} 
-		
-		if (isExcludedPage){
-			chain.doFilter(request, response); 
+		}
+
+		if (isExcludedPage) {
+			chain.doFilter(request, response);
 		} else {
-			/*String auth = req.getHeader("Authorization");
-			// 检验token是否正确
-			// 这里只是通过使用key对token进行解码是否成功，并没有对有效期、已经token里面的内容进行校验。
-			Claims claims = jwtTokenProvider.parseToken(auth);
-			
-			String token = JSONObject.toJSONString(claims);*/
+			/*
+			 * String auth = req.getHeader("Authorization"); // 检验token是否正确 //
+			 * 这里只是通过使用key对token进行解码是否成功，并没有对有效期、已经token里面的内容进行校验。 Claims claims =
+			 * jwtTokenProvider.parseToken(auth);
+			 * 
+			 * String token = JSONObject.toJSONString(claims);
+			 */
 			String token = req.getHeader("Authorization");
-			logger.info("test:"+token);
-			System.out.println("token="+token);
-			if (StringUtils.isEmpty(token) || !RedisApi.exists(token)){
+			logger.info("test:" + token);
+			System.out.println("token=" + token);
+			if (StringUtils.isEmpty(token) || !RedisApi.exists(token)) {
 				resp.sendRedirect(loginPath);
-			} else if (uri.equals("/logout")){
+			} else if (uri.equals("/logout")) {
 				TokenUtils.delToken(token);
 				chain.doFilter(request, response);
-			}else {
+			} else {
 				String value = RedisApi.get(token);
 				RedisApi.set(token, value, 1800);
-				chain.doFilter(request, response); 
+				chain.doFilter(request, response);
 			}
-			
+
 		}
-		
+
 	}
 
 	public void destroy() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
